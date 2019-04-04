29e187af5f7e6237c648e8d82ea45a3dec43be10
YosorIT <69yoyeezus69@gmail.com>
2019-04-05 02:00:41 +0300

    add changes when press on button

--- script.js
+++ script.js
@@ -11,57 +11,61 @@ class Timer {
         this.millieseconds = millieseconds;
         this.render();
     }
-    
-//метод класса запускает работу таймера при нажатии на кнопку
-    onStart() {
-        this.interval = setInterval(this.update, 1000);
-    }
-
-//метод класса завершает работу таймера при нажатии на кнопку
-    onStop() {
-        clearInterval(this.interval); 
-    }
-
-//метод класса обновляет счётчик таймера 
-    update() {
-        this.millieseconds <= 0 ? this.millieseconds -= 10 : this.onStop();
-        return currentTime();
-    }
-    
-//метод класса возвращает новое время
-    currentTime(){
-        return this.millieseconds;
-    }
-
-//метод класса отрисовывает output
+    //метод класса отрисовывает output
     createOutput() {
         this.output = document.createElement("div");
         this.output.classList.add("output");
         return this.output;
     }
 
-//метод класса отрисовывает progress bar
+    //метод класса отрисовывает progress bar
     createLine() {
         this.line = document.createElement("div");
         this.line.classList.add("line");
         return this.line;
     }
 
-//метод класса отрисовывает button
-    createButton(){
+    //метод класса отрисовывает button
+    createButton() {
         this.button = document.createElement("button");
         this.button.classList.add("btn");
-        this.button.innerHTML = "Start"; 
+        this.button.innerHTML = "Start";
+        this.button.addEventListener('click',this.toogleText.bind(this));
         return this.button;
     }
-//метод отрисовывает элементы на страницу
+    //метод класса запускает работу таймера при нажатии на кнопку
+    onStart() {
+        this.interval = setInterval(this.update, 1000);
+    }
+
+    //метод класса завершает работу таймера при нажатии на кнопку
+    onStop() {
+        clearInterval(this.interval);
+    }
+
+    //метод класса обновляет счётчик таймера 
+    update() {
+        this.millieseconds <= 0 ? this.millieseconds -= 10 : this.onStop();
+        return currentTime();
+    }
+
+    //метод класса возвращает новое время
+    currentTime() {
+        return this.millieseconds;
+    }
+
+    //метод класса меняет действие при клике на кнопку
+    toogleText(){
+        this.button.innerHTML == "Start" ? this.button.innerHTML = "Stop" : this.button.innerHTML = "Start";
+    }
+
+    //метод отрисовывает элементы на страницу
     render() {
         container.append(this.createOutput());
         container.append(this.createButton());
         container.append(this.createLine());
-    }   
+        this.width = this.line.offsetWidth;
+    }
 }
 
 new Timer(5000);
-
-
